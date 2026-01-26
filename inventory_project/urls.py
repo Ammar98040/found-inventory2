fcce23390c8e90d90fc44cec1c7782ddffe978cd
Ammar98040 <ammarnnr1@gmail.com>
2026-01-26 09:50:34 +0200

    Force Django to serve media files in production for zip deployment

--- inventory_project/urls.py
+++ inventory_project/urls.py
@@ -1,9 +1,10 @@
 """
 URL configuration for inventory_project project.
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from inventory_app import views
 
 urlpatterns = [
@@ -11,10 +12,12 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('', include('inventory_app.urls')),
+    
+    # تقديم ملفات الميديا دائماً (حتى في الإنتاج) لأننا لا نتحكم في إعدادات Nginx
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# Serve media files in development
+# Serve static files in development (Whitenoise handles this in production)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
